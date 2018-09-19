bcb10e21c899e110ee7f15e286915b3806a8f454
Aleksandar <aleksandar@decom.ba>
2018-09-19 16:49:05 +0200

    Updates: small changes - micro chng

--- JS102/app.js
+++ JS102/app.js
@@ -15,29 +15,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /********************************
  * Cloning a Node and Updating
  * Values
@@ -45,47 +22,28 @@
  *
  *******************************/
 
-// var fieldsList = document.querySelector( '.fields' ),
-//     firstField = fieldsList.children[0],
-//     firstFieldInput = firstField.querySelector( 'input' ),
-//     firstFieldIdStr = firstFieldInput.getAttribute( 'id' ),
-//     secondField = firstField.cloneNode( true ),
-//     secondFieldInput = secondField.querySelector( 'input' ),
-//     secondFieldLabel = secondField.querySelector( 'label' ),
-//     secondFieldIdStr = updateIdStr( firstFieldIdStr );
-//
-// secondFieldInput.setAttribute( 'id', secondFieldIdStr );
-// secondFieldInput.setAttribute( 'name', secondFieldIdStr );
-// secondFieldLabel.setAttribute( 'for', secondFieldIdStr );
-//
-// fieldsList.appendChild( secondField );
-//
-// function updateIdStr( value ) {
-//
-//   var strArray = value.split( '-' ),
-//       id = strArray[ strArray.length - 1 ],
-//       newId = parseInt( id ) + 1;
-//
-//   strArray[ strArray.length - 1 ] = newId;
-//   return strArray.toString().replace( ',' , '-' );
-//
-// }
+var fieldsList = document.querySelector( '.fields' ),
+    firstField = fieldsList.children[0],
+    firstFieldInput = firstField.querySelector( 'input' ),
+    firstFieldIdStr = firstFieldInput.getAttribute( 'id' ),
+    secondField = firstField.cloneNode( true ),
+    secondFieldInput = secondField.querySelector( 'input' ),
+    secondFieldLabel = secondField.querySelector( 'label' ),
+    secondFieldIdStr = updateIdStr( firstFieldIdStr );
 
+secondFieldInput.setAttribute( 'id', secondFieldIdStr );
+secondFieldInput.setAttribute( 'name', secondFieldIdStr );
+secondFieldLabel.setAttribute( 'for', secondFieldIdStr );
 
+fieldsList.appendChild( secondField );
 
+function updateIdStr( value ) {
 
+  var strArray = value.split( '-' ),
+      id = strArray[ strArray.length - 1 ],
+      newId = parseInt( id ) + 1;
 
+  strArray[ strArray.length - 1 ] = newId;
+  return strArray.toString().replace( ',', '-' );
 
-
-
-
-
-
-
-
-
-
-
-
-
-// End 1.3.11
+}
