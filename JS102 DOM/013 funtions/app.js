f7036e3bcfa639e46fa3af91c73bef9f70b7a8d7
Aleksandar <aleksandar@decom.ba>
2018-09-27 12:04:28 +0200

    Re-organisation

---
+++ JS102 DOM/013 funtions/app.js
@@ -0,0 +1,17 @@
+/********************************
+ * Functions
+ * 1.1.1.3
+ *
+ *
+ *******************************/
+
+
+
+
+function displayPost() {
+    
+    console.log( 'Function' );
+    
+}
+
+displayPost();
